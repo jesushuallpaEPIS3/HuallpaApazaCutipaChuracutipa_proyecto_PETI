1948e3b0716bf59dc8228efeea5db091edfdf01d
erick <erichuracutipa@upt.pe>
2024-10-16 18:17:59 -0500

    update infomodel.php

--- model/infoModel.php
+++ model/infoModel.php
@@ -7,14 +7,14 @@
             $this->PDO = $con->conexion();
         }
         public function insertar($id, $nombre, $descripcion){
-            $query = $this->PDO->prepare("INSERT INTO empresa(id, nombre, descripcion) values(:id,:nombre,:descripcion)");
+            $query = $this->PDO->prepare("INSERT INTO informacion(id, nombre, descripcion) values(:id,:nombre,:descripcion)");
             $query->bindParam(":nombre",$nombre);
             $query->bindParam(":descripcion",$descripcion);
             $query->bindParam(":id",$id);
             return ($query->execute()) ? true : false;
         }
         public function show($id){
-            $query = $this->PDO->prepare("Select * from empresa where id = :id limit 1");
+            $query = $this->PDO->prepare("Select * from informacion where id = :id limit 1");
             $query->bindParam(":id",$id);
             return ($query->execute()) ? $query->fetch() : false;
         }
@@ -22,14 +22,14 @@
 
         // }
         public function update($id,$nombre,$descripcion){
-            $query = $this->PDO->prepare("Update empresa SET nombre = :nombre, descripcion = :descripcion where id = :id");
+            $query = $this->PDO->prepare("Update informacion SET nombre = :nombre, descripcion = :descripcion where id = :id");
             $query->bindParam(":nombre",$nombre);
             $query->bindParam(":descripcion",$descripcion);
             $query->bindParam(":id",$id);
             return ($query->execute()) ? $id : false ;
         }
         public function comprobar($id){
-            $query = $this->PDO->prepare("Select * from empresa where id = :id");
+            $query = $this->PDO->prepare("Select * from informacion where id = :id");
             $query->bindParam(":id",$id);
             $query->execute();
             return $query->rowCount() > 0;
