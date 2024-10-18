818fac46dc877529f0c47ad9ac943d6acc65c0ee
Erick Churacutipa Blas <102677604+erick00xx@users.noreply.github.com>
2024-10-18 17:22:59 -0500

    Update header.php

--- view/head/header.php
+++ view/head/header.php
@@ -12,7 +12,7 @@
     <div class="container-fluid bg-dark p-2 mb-3">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/peti/">Inicio</a>
+      <a class="navbar-brand" href="../../">Inicio</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
