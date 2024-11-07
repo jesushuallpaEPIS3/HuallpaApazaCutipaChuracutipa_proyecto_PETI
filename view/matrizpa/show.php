306e1888b1ea4be61c594059d952ca62663773af
RicardoCutipa <rc2021069827@virtual.upt.pe>
2024-11-06 23:39:24 -0500

    #37 Avance Diseno

--- view/matrizpa/show.php
+++ view/matrizpa/show.php
@@ -11,7 +11,7 @@
 ?>
 
     <title>MATRIZ PARTICIPACION</title>
-    <link rel="stylesheet" href="../head/styles.css">
+
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <script src="script.js" defer></script>
@@ -401,6 +401,7 @@
                     <div class="quadrant-label"></div>
                 </div>
             </div>
+            <br>
             <table>
                 <thead>
                     <tr>
